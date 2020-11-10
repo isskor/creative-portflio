57be46ccfc0dbad44f2a7e4c1dc4cdae890d4822
isskor <jphuong91@gmail.com>
2020-11-10 14:07:00 +0100

    set reactRouter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from "react";
 import AboutUs from "./pages/AboutUs";
 import Nav from "./components/Nav";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
 // global styles
 import GlobalStyle from "./components/GlobalStyle";
+// Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
